e319bfa1d86c6dd92a4a23b8b8ea1315ec07f30a
Christiano Shermam <christianoshermam09@gmail.com>
2024-09-06 09:56:44 -0300

    RF05

--- src/controllers/postagensControllers.js
+++ src/controllers/postagensControllers.js
@@ -49,15 +49,17 @@ export const criar = async (request, response) => {
     }
 };
 
-export const listarPostagensid = async (request, response) => {
-    const { postagem_id } = request.params
-
+export const listarPostagensid = async (request, response) => {    
+    const { id } = request.params;
     try {
-        const postagem = await Postagens.findByPk(postagem_id)
+        idSchema.parse({ id });
+        const postagem = await Postagens.findByPk(id);
 
         if (!postagem) {
-            return response.status(404).json({ message: `Postagem ${postagem_id} não existe` });
+            return response.status(404).json({ message: 'Postagem não encontrada' });
         }
+
+        response.status(200).json(postagem);
 
         response.status(200).json({ message: postagem })
     } catch (error) {
@@ -158,3 +160,4 @@ export const excluirPostagem = async (request, response) => {
         response.status(500).json({ message: "Erro interno do servidor" });
     }
 };
+
